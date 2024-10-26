ba50a53d3a2b7b3e4457a9419ec54521d8aa50a7
intex <jerrymeletiche123@gmail.com>
2024-10-26 13:19:59 -0400

    >>>

--- hbnb_project/app/services/place_service.py
+++ hbnb_project/app/services/place_service.py
@@ -2,16 +2,16 @@ from ..models.place import Place
 from ..models import db
 
 def create_place(name, description):
-    """Crea un nuevo lugar y lo guarda en la base de datos."""
+    
     new_place = Place(name=name, description=description)
     db.session.add(new_place)
     db.session.commit()
     return new_place
 
 def get_place(place_id):
-    """Devuelve un lugar por su ID."""
+    
     return Place.query.get(place_id)
 
 def get_all_places():
-    """Devuelve todos los lugares."""
+    
     return Place.query.all()
--- hbnb_project/app/services/review_service.py
+++ hbnb_project/app/services/review_service.py
@@ -2,16 +2,16 @@ from ..models.review import Review
 from ..models import db
 
 def create_review(text, user_id, place_id):
-    """Crea una nueva reseña y la guarda en la base de datos."""
+    
     new_review = Review(text=text, user_id=user_id, place_id=place_id)
     db.session.add(new_review)
     db.session.commit()
     return new_review
 
 def get_review(review_id):
-    """Devuelve una reseña por su ID."""
+    
     return Review.query.get(review_id)
 
 def get_all_reviews():
-    """Devuelve todas las reseñas."""
+    
     return Review.query.all()
