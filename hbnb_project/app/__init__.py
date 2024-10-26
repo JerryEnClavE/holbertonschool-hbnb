63e027e6c7086346cbc18a51f6c666dc08af323e
intex <jerrymeletiche123@gmail.com>
2024-10-26 13:55:16 -0400

    >>>

--- hbnb_project/app/__init__.py
+++ hbnb_project/app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
-from .models import init_app as init_models
-from .api import register_endpoints
 
 def create_app():
     app = Flask(__name__)
+    
+    # Cargar la configuración
     app.config.from_object('config.Config')
 
-    # Inicializa los modelos
-    init_models(app)
-
-    # Registra los endpoints de la API
-    register_endpoints(app)
+    # Importar y registrar Blueprints aquí
+    # from app.api.users import users_bp
+    # app.register_blueprint(users_bp)
 
     return app
+
