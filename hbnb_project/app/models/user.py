13343d8e9e8819a45f730c72a56605a83840b190
intex <jerrymeletiche123@gmail.com>
2024-10-26 14:28:23 -0400

    >>>

--- hbnb_project/app/models/user.py
+++ hbnb_project/app/models/user.py
@@ -1,11 +1,10 @@
-from . import db
+from app import db
 
 class User(db.Model):
-    __tablename__ = 'users'
-    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.name}>'
+        return f'<User {self.username}>'
+
