a70b3dbac65ad2738028f44e8ed50ee1e40d1715
intex <jerrymeletiche123@gmail.com>
2024-10-26 14:08:13 -0400

    >>>

--- hbnb_project/app/api/__init__.py
+++ hbnb_project/app/api/__init__.py
@@ -1,11 +1,14 @@
-from flask import Blueprint
-from .users import user_bp
-from .places import place_bp
-from .reviews import review_bp
-from .amenities import amenity_bp
+from flask import Flask
 
-def register_endpoints(app):
-    app.register_blueprint(user_bp, url_prefix='/users')
-    app.register_blueprint(place_bp, url_prefix='/places')
-    app.register_blueprint(review_bp, url_prefix='/reviews')
-    app.register_blueprint(amenity_bp, url_prefix='/amenities')
+def create_app():
+    app = Flask(__name__)
+    
+    # Cargar la configuración
+    app.config.from_object('config.Config')
+
+    # Importar y registrar Blueprints aquí
+    # from app.api.users import users_bp
+    # app.register_blueprint(users_bp)
+
+    return app
+
