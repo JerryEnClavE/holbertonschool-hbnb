ce654554dfb297cadc975b1e57b69151211014f5
JerryEnClavE <jerrymeletiche123@gmail.com>
2024-11-30 18:09:25 -0400

    >>>

--- part3/config.py
+++ part3/config.py
@@ -1,16 +1,17 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    DEBUG = False
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:your_password@localhost/HBnB'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desactiva la modificación de seguimiento para optimización
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///dev.db"
-
+    SQLALCHEMY_DATABASE_URI = "mysql://root:your_password@localhost/HBnB"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desactiva el rastreo de modificaciones para evitar warnings.
 
 config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig
 }
-
